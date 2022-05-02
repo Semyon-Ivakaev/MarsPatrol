33d12777331ef92362b79f8b3a8692f120af65dc
Semyon-Ivakaev <stixonforpython@gmail.com>
2022-05-02 14:59:05 +0300

    MP-29

--- app/src/main/java/com/vertigo/marspatrol/presentation/MainActivity.kt
+++ app/src/main/java/com/vertigo/marspatrol/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.vertigo.marspatrol
+package com.vertigo.marspatrol.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.vertigo.marspatrol.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

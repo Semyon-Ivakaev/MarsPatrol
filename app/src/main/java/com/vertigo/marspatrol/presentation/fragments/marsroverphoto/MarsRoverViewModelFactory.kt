d20145b9e8ec6cfc5ad55ab6e3c4995b04b218e9
Semyon-Ivakaev <stixonforpython@gmail.com>
2022-07-10 23:13:04 +0300

    MP-42

--- app/src/main/java/com/vertigo/marspatrol/presentation/fragments/marsroverphoto/MarsRoverViewModelFactory.kt
+++ app/src/main/java/com/vertigo/marspatrol/presentation/fragments/marsroverphoto/MarsRoverViewModelFactory.kt
@@ -3,17 +3,12 @@ package com.vertigo.marspatrol.presentation.fragments.marsroverphoto
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.vertigo.marspatrol.data.remotesource.repository.MarsRoverPhotoRepositoryImpl
-import com.vertigo.marspatrol.domain.usecase.marsphoto.GetDefaultMarsPhotoListUseCase
 import com.vertigo.marspatrol.domain.usecase.marsphoto.GetDefaultSolForMarsRoverUseCase
 import com.vertigo.marspatrol.domain.usecase.marsphoto.GetNeededMatsPhotoListUseCase
 
 class MarsRoverViewModelFactory: ViewModelProvider.Factory {
     private val marsRoverPhotoRepositoryImpl by lazy {
         MarsRoverPhotoRepositoryImpl()
-    }
-
-    private val getDefaultMarsPhotoListUseCase by lazy {
-        GetDefaultMarsPhotoListUseCase(marsRoverPhotoRepository = marsRoverPhotoRepositoryImpl)
     }
 
     private val getNeededMatsPhotoListUseCase by lazy {
@@ -26,7 +21,6 @@ class MarsRoverViewModelFactory: ViewModelProvider.Factory {
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return MarsRoverPhotoViewModel(
-            getDefaultMarsPhotoListUseCase = getDefaultMarsPhotoListUseCase,
             getNeededMatsPhotoListUseCase = getNeededMatsPhotoListUseCase,
             getDefaultSolForMarsRoverUseCase = getDefaultSolForMarsRoverUseCase) as T
     }

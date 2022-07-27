5ef2c0075bb70880cb24f2562f5d7cbc1c71217b
Semyon-Ivakaev <stixonforpython@gmail.com>
2022-07-27 23:58:00 +0300

    MP-27

--- app/src/main/java/com/vertigo/marspatrol/presentation/fragments/favorites/FragmentFavorites.kt
+++ app/src/main/java/com/vertigo/marspatrol/presentation/fragments/favorites/FragmentFavorites.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.DefaultItemAnimator
@@ -33,11 +34,34 @@ class FragmentFavorites: Fragment() {
     ): View? {
         _binding = FragmentFavoritesBinding.inflate(inflater, container, false)
         createRecycler()
+        initPullToRefresh()
+
+        favoritesViewModel.getFavoritePhotosList()
+
         favoritesViewModel.favoritePhotosViewModel.observe(viewLifecycleOwner, {
             result -> updateRecycler(result)
         })
 
         return binding.root
+    }
+
+
+    private fun initPullToRefresh() {
+        with(binding) {
+            swipeRefresh.setOnRefreshListener {
+                clearRecycler()
+                showProgressBar(show = true)
+                favoritesViewModel.getFavoritePhotosList()
+                swipeRefresh.isRefreshing = false
+            }
+        }
+    }
+
+
+    private fun clearRecycler() {
+        with(binding) {
+            photoRecycler.layoutManager = null
+        }
     }
 
     private fun createRecycler() {
@@ -58,6 +82,20 @@ class FragmentFavorites: Fragment() {
             photoRecycler.adapter = marsRoverPhotoAdapter
             photoRecycler.layoutManager = layoutManager
             photoRecycler.itemAnimator = DefaultItemAnimator()
+        }
+    }
+
+    private fun showErrorMessage(show: Boolean, messageText: String?) {
+        with(binding) {
+            errorMessageTextview.text = messageText
+            errorMessageCardview.isVisible = show
+        }
+    }
+
+    private fun showProgressBar(show: Boolean) {
+        Log.v("App", "showProgressBar $show")
+        with(binding) {
+            progressBar.isVisible = show
         }
     }
 
